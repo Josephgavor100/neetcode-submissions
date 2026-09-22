4c34e92133cfc9bbc9837604954274dc86ff24ee
Joe G. <josephgavor100@gmail.com>
2026-09-22 22:03:46 +0000

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.strip().lower().replace(" ", "")
+        s = "".join([c for c in s if c.isalnum()])
+        return s[::-1] == s
+
+
+        
+        
